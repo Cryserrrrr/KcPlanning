494e04da51022cda4d911d544d7d1d3578afcb72
Cryserrrrr <yoti3005@gmail.com>
2025-03-16 23:39:53 +0100

    debug prod

--- app/utils/scraper/riotMatchScraper.ts
+++ app/utils/scraper/riotMatchScraper.ts
@@ -32,6 +32,12 @@ export const riotMatchScraper = async ({
   const page = await browser.newPage();
   let eventsData: any[] = [];
 
+  await page.evaluateOnNewDocument(() => {
+    Object.defineProperty(navigator, "webdriver", { get: () => false });
+  });
+
+  await page.setViewport({ width: 1920, height: 1080 });
+
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
   );
